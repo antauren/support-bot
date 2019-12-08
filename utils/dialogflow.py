7941b9f55510be61e7a563ee5cab90db9852cbe6
antauren <antauren@yandex.ru>
2019-12-08 16:32:49 +0300

    add detect_intent_text

--- utils/dialogflow.py
+++ utils/dialogflow.py
@@ -4,6 +4,34 @@ import dialogflow_v2 as dialogflow
 from google.protobuf.json_format import MessageToDict
 
 
+def detect_intent_text(project_id, session_id, text, language_code) -> dict:
+    res = detect_intent_texts(project_id, session_id, [text], language_code)
+
+    return list(res)[0]
+
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    for text in texts:
+        text_input = dialogflow.types.TextInput(text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(session=session, query_input=query_input)
+
+        yield {'query': response.query_result.query_text,
+               'answer': response.query_result.fulfillment_text,
+               'display_name': response.query_result.intent.display_name,
+               'confidence': response.query_result.intent_detection_confidence
+               }
+
+
 def get_intent(project_id: str, display_name: str) -> dict:
     client = dialogflow.IntentsClient()
     parent = client.project_agent_path(project_id)
